3be0e62c73bc08668f4d52c243581ebbe03999b0
Thejaswi-Gowda <thejaswipgowda3180@gmail.com>
2024-02-12 01:43:09 +0530

    Implemented Route functinality for Application.

--- Food-App/src/App.js
+++ Food-App/src/App.js
@@ -2,20 +2,44 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Header from './Components/Header';
 import Body from './Components/Body';
+import About from './Components/About';
+import Contact from './Components/Contact';
+import ErrorPage from './Components/ErrorPage';
+import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom';
 
-let heading = React.createElement('h1', {}, "This is heading");
-
-const Heading = () => (
+const AppLayout = () => (
     <>
         <div className="header-container">
             <Header/>
         </div>
         <div className='body-container'>
-            <Body/>
+            <Outlet />
         </div>
     </>
 )
 
+const appRouter = createBrowserRouter([
+    {
+        path: "/",
+        element: <AppLayout />,
+        children: [
+            {
+                path: "/",
+                element: <Body />,
+            },
+            {
+                path: "/about",
+                element: <About />,
+            },
+            {
+                path: "/contact",
+                element: <Contact />,
+            },
+        ],
+        errorElement: <ErrorPage />
+    },
+]);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-root.render(<Heading/ >);
+root.render(<RouterProvider router={appRouter} />);
---
+++ Food-App/src/Components/ErrorPage.js
@@ -0,0 +1,19 @@
+import { useRouteError } from "react-router-dom";
+
+const ErrorPage = () => {
+    const err = useRouteError();
+    // console.log(err);
+    return (
+        <div>
+            <h1>Opps</h1>
+            <h2>Something went wrong!!</h2>
+            <h4>{err.data}</h4>
+            <h4>{err.error.message}</h4>
+            <h4>{err.internal}</h4>
+            <h4>{err.status}</h4>
+            <h4>{err.statusText}</h4>
+        </div>
+    )
+}
+
+export default ErrorPage;
